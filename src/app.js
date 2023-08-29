c0ab24f8e665ece149625fff56c3286db47e0951
WILLIAM NICOLAS BUITRAGO CAMACHO <WNBUITRA@davivienda.com>
2023-08-29 18:54:45 -0500

    Configurar rutas estáticas  para implementación de y uso de imágenes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes/routes");
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '')));
 
 // Create an instance of exphbs
 const hbs = exphbs.create();
